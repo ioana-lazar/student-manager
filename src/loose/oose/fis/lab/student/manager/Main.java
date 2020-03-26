6115985f7279281708174e54fc89acb2effa96e2
Ioana Lazar <ioanalazar2008@yahoo.com>
2020-03-26 14:56:35 +0200

    Changed sm-1

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -11,15 +11,10 @@ public class Main extends Application {
     @Override
 
     public void start(Stage primaryStage) throws Exception {
-
         Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
-
         primaryStage.setTitle("FIS - Student Manager");
-
         primaryStage.setScene(new Scene(root, 600, 400));
-
         primaryStage.show();
-
     }
 
 
